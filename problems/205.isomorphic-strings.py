502c604e0d1600e9d8fe5262ff070dd24c71c445
khalil hadji <h-adji_tech@proton.me>
2025-02-11 21:07:12 +0100

    isomorphic strings

--- problems/205.isomorphic-strings.py
+++ problems/205.isomorphic-strings.py
@@ -3,60 +3,59 @@
 # Percent: 46.264793%
 
 
-
 # Given two strings s and t, determine if they are isomorphic.
-# 
+#
 # Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-# 
+#
 # All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-# 
-#  
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: s = "egg", t = "add"
-# 
+#
 # Output: true
-# 
+#
 # Explanation:
-# 
+#
 # The strings s and t can be made identical by:
-# 
-# 
+#
+#
 # 	Mapping 'e' to 'a'.
 # 	Mapping 'g' to 'd'.
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: s = "foo", t = "bar"
-# 
+#
 # Output: false
-# 
+#
 # Explanation:
-# 
+#
 # The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: s = "paper", t = "title"
-# 
+#
 # Output: true
-# 
-# 
-#  
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 	1 <= s.length <= 5 * 10⁴
 # 	t.length == s.length
 # 	s and t consist of any valid ascii character.
-# 
- 
+#
+
 
 class Solution(object):
     def isIsomorphic(self, s, t):
@@ -65,4 +64,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
+
+        def asym_isomorph(s, t):
+            d = {}
+            for i in range(len(s)):
+                if s[i] not in d:
+                    d[s[i]] = t[i]
+                else:
+                    if d[s[i]] == t[i]:
+                        continue
+                    else:
+                        return False
+            return True
+
+        return asym_isomorph(s, t) and asym_isomorph(t, s)
