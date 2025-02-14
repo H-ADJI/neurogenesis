1d942f938aa8010e9095963cf513fab38584a38c
khalil hadji <h-adji_tech@proton.me>
2025-02-14 07:48:48 +0100

    single number

---
+++ problems/136.single-number.py
@@ -0,0 +1,52 @@
+# Category: algorithms
+# Level: Easy
+# Percent: 75.08407%
+
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+#
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [2,2,1]
+#
+# Output: 1
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [4,1,2,1,2]
+#
+# Output: 4
+#
+#
+# Example 3:
+#
+#
+# Input: nums = [1]
+#
+# Output: 1
+#
+#
+#
+# Constraints:
+#
+#
+# 	1 <= nums.length <= 3 * 10⁴
+# 	-3 * 10⁴ <= nums[i] <= 3 * 10⁴
+# 	Each element in the array appears twice except for one element which appears only once.
+#
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for e in nums:
+            res = res ^ e
+        return res
