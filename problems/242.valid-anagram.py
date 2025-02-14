8c17c6a3b30e9813cab53b820909b84d13494300
khalil hadji <h-adji_tech@proton.me>
2025-02-14 17:43:16 +0100

    anagrams in 3 ways

---
+++ problems/242.valid-anagram.py
@@ -0,0 +1,65 @@
+# Category: algorithms
+# Level: Easy
+# Percent: 65.98075%
+
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "anagram", t = "nagaram"
+#
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: s = "rat", t = "car"
+#
+# Output: false
+#
+#
+#
+# Constraints:
+#
+#
+# 	1 <= s.length, t.length <= 5 * 10⁴
+# 	s and t consist of lowercase English letters.
+#
+#
+#
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        base = ord("a")
+        map = [0 for _ in range(26)]
+        for e in t:
+            idx = ord(e) - base
+            map[idx] += 1
+        for e in s:
+            idx = ord(e) - base
+            map[idx] -= 1
+        for i in map:
+            if i != 0:
+                return False
+        return True
+        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
+        counter = Counter(s)
+        for i in t:
+            if i not in counter:
+                return False
+            if counter[i] == 0:
+                return False
+            else:
+                counter[i] -= 1
+        return True
