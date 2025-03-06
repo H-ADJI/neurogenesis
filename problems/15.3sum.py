22777f02cb377da83731d7644fcc197ed89dfe5c
khalil hadji <h-adji_tech@proton.me>
2025-03-06 16:35:11 +0100

    3sum solution

--- problems/15.3sum.py
+++ problems/15.3sum.py
@@ -50,18 +50,26 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         results = []
-        for i in range(len(nums)):
-            target = nums[i]
-            if i > 0 and target == nums[i - 1]:
+        print(nums)
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            if i > 0 and a == nums[i - 1]:
                 continue
             j = i + 1
             k = len(nums) - 1
             while j < k:
-                if i == j or nums[j] + nums[k] + target < 0:
+                b = nums[j]
+                c = nums[k]
+                three_sum = a + b + c
+                if three_sum < 0:
                     j += 1
-                elif i == k or nums[j] + nums[k] + target > 0:
+                elif three_sum > 0:
                     k -= 1
-                if nums[k] + target + nums[j] == 0:
-                    results.append([target, nums[k], nums[j]])
-                    break
+                if three_sum == 0:
+                    results.append([a, b, c])
+                    j += 1
+                    k -= 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
         return results
