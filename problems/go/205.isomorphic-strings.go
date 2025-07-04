22aab4a3152cec7743920cc6129dd9e2f627ca16
khalil hadji <h-adji_tech@proton.me>
2025-07-04 18:49:18 +0200

    isomorphic strings

---
+++ problems/go/205.isomorphic-strings.go
@@ -0,0 +1,76 @@
+// Category: algorithms
+// Level: Easy
+// Percent: 46.747112%
+
+// Given two strings s and t, determine if they are isomorphic.
+//
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+//
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+//
+//
+// Example 1:
+//
+//
+// Input: s = "egg", t = "add"
+//
+// Output: true
+//
+// Explanation:
+//
+// The strings s and t can be made identical by:
+//
+//
+// 	Mapping 'e' to 'a'.
+// 	Mapping 'g' to 'd'.
+//
+//
+//
+// Example 2:
+//
+//
+// Input: s = "foo", t = "bar"
+//
+// Output: false
+//
+// Explanation:
+//
+// The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+//
+//
+// Example 3:
+//
+//
+// Input: s = "paper", t = "title"
+//
+// Output: true
+//
+//
+//
+// Constraints:
+//
+//
+// 	1 <= s.length <= 5 * 10⁴
+// 	t.length == s.length
+// 	s and t consist of any valid ascii character.
+//o
+// solved using tanslation dict
+
+package main
+
+func isIsomorphic(s string, t string) bool {
+	asymIsomorphic := func(s, t string) bool {
+		trs := make(map[byte]byte)
+		for i, _ := range s {
+			if c, ok := trs[s[i]]; ok {
+				if c != t[i] {
+					return false
+				}
+			} else {
+				trs[s[i]] = t[i]
+			}
+		}
+		return true
+	}
+	return asymIsomorphic(s, t) && asymIsomorphic(t, s)
+}
