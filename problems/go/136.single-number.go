3b1f7aa06b729823ecfd5533575367d6e887d1f8
khalil hadji <h-adji_tech@proton.me>
2025-07-04 18:33:10 +0200

    single number

---
+++ problems/go/136.single-number.go
@@ -0,0 +1,52 @@
+// Category: algorithms
+// Level: Easy
+// Percent: 75.821335%
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+//
+// Example 1:
+//
+//
+// Input: nums = [2,2,1]
+//
+// Output: 1
+//
+//
+// Example 2:
+//
+//
+// Input: nums = [4,1,2,1,2]
+//
+// Output: 4
+//
+//
+// Example 3:
+//
+//
+// Input: nums = [1]
+//
+// Output: 1
+//
+//
+//
+// Constraints:
+//
+//
+// 	1 <= nums.length <= 3 * 10⁴
+// 	-3 * 10⁴ <= nums[i] <= 3 * 10⁴
+// 	Each element in the array appears twice except for one element which appears only once.
+//
+// xor cancels out repeated elements
+
+package main
+
+func singleNumber(nums []int) int {
+	single := 0
+	for _, v := range nums {
+		single ^= v
+	}
+	return single
+}
